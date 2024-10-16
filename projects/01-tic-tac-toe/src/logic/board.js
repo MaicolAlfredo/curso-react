3a510f106da11efda239c1e7cf3d18f2df8167ee
MaicolAlfredo <tegnoblog.17alfrdo@gmail.com>
2024-10-16 21:45:34 +0000

    clase tic-tac-toe

---
+++ projects/01-tic-tac-toe/src/logic/board.js
@@ -0,0 +1,15 @@
+import { WINNER_COMBO } from "../constants";
+
+export const checkWinnerFrom = (boardToCheck) => {
+  for (const combo of WINNER_COMBO) {
+    const [a, b, c] = combo;
+    if (
+      boardToCheck[a] &&
+      boardToCheck[a] === boardToCheck[b] &&
+      boardToCheck[a] === boardToCheck[c]
+    ) {
+      return boardToCheck[a];
+    }
+  }
+  return null;
+};
