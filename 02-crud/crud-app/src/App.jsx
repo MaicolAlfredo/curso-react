903572cfc70d0186848f29b34e929d557e3771ed
MaicolAlfredo <tegnoblog.17alfrdo@gmail.com>
2025-07-14 17:03:34 +0000

    Agregar configuración inicial de la aplicación CRUD con React y Vite, incluyendo componentes, estilos y configuración de ESLint.

---
+++ 02-crud/crud-app/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+import CrudApp from './components/CrudApp'
+
+function App() {
+  
+
+  return (
+    <>
+      <h2>CRUD APP</h2>
+      <CrudApp/>
+    </>
+  )
+}
+
+export default App
---
+++ 02-crud/crud-app/src/components/CrudApp.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import CrudForm from "./CrudForm";
+import CrudTable from "./CrudTable";
+
+const initialDb = [
+  {
+    id: 1,
+    name: "Seiya",
+    constellation: "Pegaso",
+  },
+  {
+    id: 2,
+    name: "Shiryu",
+    constellation: "Dragón",
+  },
+  {
+    id: 3,
+    name: "Hyoga",
+    constellation: "Cisne",
+  },
+  {
+    id: 4,
+    name: "Shun",
+    constellation: "Andromeda",
+  },
+  {
+    id: 5,
+    name: "Ikki",
+    constellation: "Fenix",
+  },
+];
+
+const CrudApp = () => {
+  const [db, setDb] = useState(initialDb);
+  const [dataToEdit, setDataToEdit] = useState(null);
+
+  const createData = (data) => {
+    data.id = Date.now();
+    setDb([...db, data]);
+  };
+
+  const updateData = (data) => {
+    let newData = db.map((el) => (el.id === data.id ? data : el));
+    setDb(newData);
+
+    setDb(newData);
+  };
+
+  const deleteData = (id) => {};
+
+  return (
+    <>
+      <h2>CRUD APP</h2>
+      <CrudForm
+        createData={createData}
+        updateData={updateData}
+        dataToEdit={dataToEdit}
+        setDataToEdit={setDataToEdit}
+      />
+      <CrudTable
+        data={db}
+        setDataToEdit={setDataToEdit}
+        deleteData={deleteData}
+      />
+    </>
+  );
+};
+
+export default CrudApp;
